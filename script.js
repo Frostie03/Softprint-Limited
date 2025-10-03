c64d1948512ffde867eed168864abfaea28e9d05
Nikki <142841528+Frostie03@users.noreply.github.com>
2025-10-03 17:46:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// function to show the sidebar
+function showSidebar(){
+  const sidebar = document.querySelector('.side-bar')
+  sidebar.style.display = 'flex'
+}
+// function to hide the sidebar
+function hideSidebar(){
+  const sidebar = document.querySelector('.side-bar')
+  sidebar.style.display = 'none'
+}
